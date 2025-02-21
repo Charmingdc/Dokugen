38899063ecf8d7e0521c28530f4078d4b3ff390c
Adebayo Muis <charmingdc002@gmail.com>
2025-02-21 15:07:26 +0100

    Redesign home page

--- documentation/src/pages/Home.tsx
+++ documentation/src/pages/Home.tsx
@@ -1,26 +1,33 @@
 import { LiaAngleRightSolid } from "react-icons/lia";
+import { SlCloudDownload } from "react-icons/sl";
+
 
 const Home = () => {
  return (
    <section className='home-section'>
-    <h1> Dokugen </h1>
-    <p>
-     Your one stop CLI tool to automatically generate high-quality README files for your projects. Spend less time on documentation and more time building! 🚀
-    </p>
-    
-    <div className='install-bar'>
-     <span>
-      <LiaAngleRightSolid size={22} />
-     </span>
+     <div className='brief-info'>
+       <h1> Dokugen </h1>
+       <p>
+         Your one stop CLI tool to automatically generate high-quality README files for your projects. Spend less time on documentation and more time building! 🚀
+       </p>
+       
+       <button>
+         Download Dokugen 
+         <SlCloudDownload />
+       </button>
+       
+       <p>
+        <strong> Dokugen </strong> can also be installed via
+         <a href='/' target='_blank'>
+         <strong>
+           version managers like npm.
+         </strong>
+        </a>
+       </p>
+     </div>
      
-     <span>
-       $ npm install -g dokugen 
-     </span>
-    </div>
-    
-    <img 
-      src='developer-activity.png' 
-      alt='Developer' />
+     <div className='home-codebox'>
+     </div>
    </section>
  )
 }
